36f7b84a15358333c07207fc18e1cb83318d515e
Oleksandr Buchek <oleksandr.buchek@tasconline.com>
2020-11-01 11:27:51 -0600

    develop

--- src/app/modules/weather/weather.module.ts
+++ src/app/modules/weather/weather.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { WeatherListContainerComponent } from './containers/weather-list/weather-list-container.component';
 import { WeatherItemContainerComponent } from './containers/weather-item/weather-item-container.component';
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../../shared/shared.module';
 import { WeatherListComponent } from './components/weather-list/weather-list.component';
 
 
